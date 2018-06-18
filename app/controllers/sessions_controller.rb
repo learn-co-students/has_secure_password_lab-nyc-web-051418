125001ae7dafcd5a9d4ab81426427977f0df4ca8
github username <github email address>
2018-06-17 23:04:46 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,14 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:user][:name])
 
-    return head(:forbidden) unless @user.authenticate(password_digest: params[:user][:password])
-    session[:user_id] = @user.id
+    session[:user_id] = @user.id if @user.try(:authenticate, params[:user][:password_digest])
+
+  #   else
+  #     redirect_to '/sessions/new'
+  #   #return head(:forbidden) unless user_authentication_result
+  # end
+
+
 
   end
 
